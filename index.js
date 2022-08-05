27fc1b3869d0f4710c85e0ef15cc0905a49ff8dd
Anderick9 <georgiou.andreas.18@gmail.com>
2022-08-06 00:37:33 +0800

    hi

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-var x = (Math.floor(Math.random()*3))
+
+/*rock=1 paper=0 scissors=2*/
+function press(Number) {
+   { var x = (Math.floor(Math.random()*3))
 
 const getComputerChoice=()=>{
 if (x===0)
@@ -6,33 +9,24 @@ return "paper"
 else if (x===1)
 return "rock"
 else 
-return "scissors";}
+return "scissors";}}
 
-
-console.log(x)
-console.log(getComputerChoice())
-
-/*rock=1 paper=0 scissors=2*/
-function press(Number) {
-    const z=Number;
+const z=Number;
     console.log(z);
 {if (x==0 && z==2)
-alert ("Pc chose paper! So you win what a legend, refresh to play again");
+alert ("Pc chose paper! So you win what a legend, chop chop butmunch");
 else if (x==1 && z==0)
-alert ("Pc chose rock! So you win what a legend, refresh to play again");
+alert ("Pc chose rock! So you win what a legend, you wrapped that bitch up");
 else if (x==2 && z==1)
-alert ("Pc chose scissors! So you win what a legend, refresh to play again");
+alert ("Pc chose scissors! So you win what a legend, ROCK SMASH");
 else if(z==2 && x==1 )
-alert ("Pc chose rock! So you lose, refresh to try again");
+alert ("Pc chose rock! So you lose, how does rock not always win");
 else if(z==0 && x==2)
-alert ("Pc chose scissors! So you lose, refresh to try again");
+alert ("Pc chose scissors! So you lose, you chose a piece of paper what did you except");
 else if (z==1 && x==0)
-alert ("Pc chose paper! So you lose, refresh to try again");
+alert ("Pc chose paper! So you lose, thats hilarious you got beaten by a piece of paper");
 else if (x==z)
-alert ("Draw, refresh to try again ");}
-
-
-};
+alert ("Draw-- As in the pc chose the same, not that you have to draw anything");}};
 
 
 
